640a6b99ab64248582d046ceaba010db7efb7cc8
Oleg Yarushin <felix87_87@mail.ru>
2024-02-05 22:25:28 +0300

    post list view & template add

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'status', 'updated', 'published', 'created']
+    list_filter = ['author', 'status', 'updated']
+    search_fields = ['title', 'body']
+    prepopulated_fields = { 'slug': ('title',) }
+    date_hierarchy = 'published'
+    ordering = ['status', 'published', 'created', 'title', 'author']
