5c2ac533209e28b56243e953da9eefdf7520987a
Oleg Yarushin <felix87_87@mail.ru>
2024-02-06 21:20:54 +0300

    add detail view, publishmanager, config pre-commit hooks

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,28 +2,31 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+from blog.manager import PublishedManeger
+
 
 class Post(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DRAFT', 'Draft'
-        PUBLISHED = 'PUB', 'Published'
+        DRAFT = "DRAFT", "Draft"
+        PUBLISHED = "PUB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=100)
     body = models.TextField()
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.DRAFT)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
     published = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    objects = models.Manager()
+    approved = PublishedManeger()
+
     class Meta:
-        ordering = ['-published']
-        indexes = [
-            models.Index(fields=['-published'])
-        ]
+        ordering = ["-published"]
+        indexes = [models.Index(fields=["-published"])]
 
     def __str__(self):
         return self.title
--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -12,18 +12,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1)xv1_c*3niqrqf&7)0m)isnoy3_pvn87ycq-6pl(x017c-2$$"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
