649fd17c2ca66640d5961aa7a79366b7988ec366
Oleg Yarushin <felix87_87@mail.ru>
2024-02-12 22:26:05 +0300

    comment.system

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,13 +1,36 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Comment, Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'status', 'updated', 'published', 'created']
-    list_filter = ['author', 'status', 'updated']
-    search_fields = ['title', 'body']
-    prepopulated_fields = { 'slug': ('title',) }
-    date_hierarchy = 'published'
-    ordering = ['status', 'published', 'created', 'title', 'author']
+    list_display = [
+        "title",
+        "slug",
+        "author",
+        "status",
+        "updated",
+        "published",
+        "created",
+    ]
+    list_filter = ["author", "status", "updated"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "published"
+    ordering = ["status", "published", "created", "title", "author"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "post",
+        "user",
+        "body",
+        "created",
+        "updated",
+    ]
+    list_filter = ["created", "updated"]
+    search_fields = ["body"]
+    date_hierarchy = "updated"
+    ordering = ["post", "user", "body", "created", "updated"]
---
+++ src/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from blog.models import Comment
+
+
+class EmailPostForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(widget=forms.Textarea, required=False)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["body"]
